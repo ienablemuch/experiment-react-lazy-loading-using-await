de0bf2280706ff0d69d8dd9cce06979d0ada0025
Michael Buen <me@ienablemuch.com>
2019-08-05 21:31:34 +0800

    Verified that await can be used instead of Promise-then pattern

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [sum, setSum] = useState(0);
+    const [concatenated, setConcatenated] = useState("");
+
+    useEffect(() => {
+        async function processData() {
+            const [math, util] = await Promise.all([
+                import("./math" /* webpackChunkName: "math" */),
+                import("./util" /* webpackChunkName: "util" */)
+            ]);
+
+            const sum = math.add(7, 6);
+            const concatenated = util.concat(7, 6);
+
+            setSum(sum);
+            setConcatenated(concatenated);
+        }
+
+        processData();
+    });
+
+    return (
+        <div className="App">
+            Sum: {sum}
+            <br />
+            Concatenated: {concatenated};
+        </div>
+    );
 }
 
 export default App;
